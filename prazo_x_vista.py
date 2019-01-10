98b36de817082aeba2253bee18c4187dfc207604
Andre Ferreira Guimaraes Junior <andrefgj@br.ibm.com>
2019-01-10 01:37:58 -0200

    sumário detalhado

--- prazo_x_vista.py
+++ prazo_x_vista.py
@@ -1,9 +1,13 @@
 import array
 
+# Flags
+help = False
 parcelas = 12
 selic = 6.5
 total_a_prazo = 1305.63
 total_a_vista = 1109.78
+detailed = False
+
 
 
 
@@ -36,20 +40,21 @@ for i in range(1, parcelas):
         prazo.insert(i,[saldo, rendimento_mensal])
 
 
-for j in range(parcelas):
-    if j == 0:
+for i in range(parcelas):
+    if i == 0:
         saldo = desconto_a_vista
         rendimento_mensal = saldo / 100 * taxa_poupanca
         vista.insert(0, [saldo, rendimento_mensal])
     else:
-        saldo = vista[j - 1][0] + vista[j - 1][1] 
+        saldo = vista[i - 1][0] + vista[i - 1][1] 
         rendimento_mensal = saldo / 100 * taxa_poupanca
-        vista.insert(j, [saldo, rendimento_mensal])
+        vista.insert(i, [saldo, rendimento_mensal])
 
 
 rendimento_a_prazo = prazo[parcelas - 2][0] + prazo[parcelas - 2][1]
 rendimento_a_vista = vista[parcelas -1][0] + vista[parcelas - 1][1]
 
+# Exibir sumário
 print('Taxa de Rendimento poupança: ' + str(taxa_poupanca))
 print('Valor Total à vista: ' + str(total_a_vista))
 print('Valor Total à prazo: ' + str(total_a_prazo))
@@ -58,24 +63,22 @@ print('Rendimento poupança a prazo: ' + str(format(rendimento_a_prazo, '.2f')))
 print('Rendimento poupança a vista: ' + str(format(rendimento_a_vista, '.2f')))
 print('\n')
 
+if detailed == True:
+    print('Prospecção de Pagamento à Prazo')
+    print('-------------------------------')
+    print('Parcela N. \t Saldo \t Rendimento Mensal')
+    print('1 \t\t' + str(format(total_a_vista)) + '\t\t' + str(format(0, '.2f')))
+    for i in range(len(prazo)):
+        print(str(i + 2) + '\t\t' + str(format(prazo[i][0], '.2f')) + '\t\t' + str(format(prazo[i][1], '.2f')))
 
-
-print('Prospecção de Pagamento à Prazo')
-print('-------------------------------')
-print('Parcela N. \t Saldo \t Rendimento Mensal')
-print('1 \t\t' + str(format(total_a_vista)) + '\t\t' + str(format(0, '.2f')))
-for i in range(len(prazo)):
-    print(str(i + 2) + '\t\t' + str(format(prazo[i][0], '.2f')) + '\t\t' + str(format(prazo[i][1], '.2f')))
-
-print('\n')
-print('Prospecção de Pagamento à Vista')
-print('-------------------------------')
-print('Parcela N. \t Saldo \t Rendimento Mensal')
-# print('1 \t\t' + str(format(total_a_vista)) + '\t\t' + str(format(0, '.2f')))
-for i in range(len(vista)):
-    print(str(i + 1) + '\t\t' + str(format(vista[i][0], '.2f')) + '\t\t' + str(format(vista[i][1], '.2f')))
-
-print('\n')
+    print('\n')
+    print('Prospecção de Pagamento à Vista')
+    print('-------------------------------')
+    print('Parcela N. \t Saldo \t Rendimento Mensal')
+    # print('1 \t\t' + str(format(total_a_vista)) + '\t\t' + str(format(0, '.2f')))
+    for i in range(len(vista)):
+        print(str(i + 1) + '\t\t' + str(format(vista[i][0], '.2f')) + '\t\t' + str(format(vista[i][1], '.2f')))
+    print('\n')
 
 if rendimento_a_prazo > rendimento_a_vista:
     print('#########################################')
